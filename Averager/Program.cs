004381f610601f35f888ed07b0a4bab1101faa2a
kimikomu <kimikomu@gmail.com>
2017-05-21 20:12:34 -0700

    Initial commit was made with an empty Program file. This is the correct file.

--- Averager/Program.cs
+++ Averager/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Averager
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
+			var counter = 0;
+			var runningTotal = 0.0;
+			var average = 0.0;
+
+			while (true)
+			{
+				// repetedly prompt the user for numbers
+				Console.Write("Please enter a number or enter \"done\" to see average: ");
+
+				var input = Console.ReadLine();
+
+				// on imediate exit of the program, counter is 1 to make average 0
+				if (input.ToLower() == "done" && counter == 0)
+				{
+					counter = 1;
+					break;
+				}
+				else if (input.ToLower() == "done")
+				{
+					break;
+				}
+
+				try
+				{
+					var number = double.Parse(input);
+
+					// add numbers entered together
+					runningTotal += number;
+					counter += 1;
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("Invalid Input");
+					continue;
+				}
+			}
+
+			// get average of numbers entered
+			average = runningTotal / counter;
+
+			while (true)
+			{
+				Console.Write("Thre average of the numbers entered is " + average + ". Enter \"quit\" to exit: ");
+				var exitInput = Console.ReadLine();
+				if (exitInput.ToLower() == "quit")
+				{
+					break;
+				}
+			}
 		}
 	}
 }
